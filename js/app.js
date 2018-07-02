b6a6c1e8862cbfc8feb424df8c175eaa47a57758
Jay Escala <Jonathan.J.Escala@gmail.com>
2018-07-02 14:53:52 -0500

    Commit 3 - Added game physics and controls.

--- js/app.js
+++ js/app.js
@@ -4,69 +4,92 @@ const canvas = document.getElementById("canvas");
 
 const ctx = canvas.getContext("2d");
 
+// Controls
 let name = "Player Name";
 let height = 100;
 let width = 50;
-let x = 0;
-let y = 300;//canvas.height - height;
+let fps = 0;
+let posX = 0;
+let posY = canvas.height - height;
+let velX = 0;
+let velY = 0;
+let accX = 0;
+let accY = 0;
+let gravY = 1;
+let dragX = 0.5;
+let jumpY = 15;
 
-// const draw = () =>{
-	// ctx.beginPath();
-	// ctx.rect(x, y, width, height);
-	// ctx.fillStyle = "white";
-	// ctx.fill();
-	// ctx.closePath();
-// }
+// Frame Rendering
+const renderPage = () => {
+	update();
+	draw();
+	fps = window.requestAnimationFrame(renderPage);
+}
 
-const draw = () =>{
+const draw = () => {
 	canvas.width = canvas.width;
-	ctx.rect (x, y, width, height);
+	ctx.rect(posX, posY, width, height);
 	ctx.stroke();
+}
+
+const update = () => {
+	if(velX > 0){
+		velX += -dragX;
+	} else if(velX < 0){
+		velX += dragX;
+	}
+
+	posX += velX;
+
+	if(posX > canvas.width - width){
+		posX = canvas.width - width;
+		velX = 0;
+	}
+	if(posX < 0){
+		posX = 0;
+		velX = 0;
+	}
+
+	velY += gravY;
+	posY += velY;
+
+	if(posY > canvas.height - height){
+		posY = canvas.height - height;
+		velY = 0;
+	}
+}
+
+// Actions
+
+const jump = () => {
+	velY += -jumpY;
 }
 
 const move = (event) => {
 	console.log(event.keyCode);
 	if(event.keyCode == 39){
 		console.log("Right");
-		if(x <= canvas.width - width){
-			x += 5;
+		if(posX <= canvas.width - width){
+			velX += 10;
 		} else {
-			x = 0;
+			posX = 0;
 		}
 	}
 	if(event.keyCode == 37){
 		console.log("Left");
-		if(x >= 0){
-			x += -5;
+		if(posX >= 0){
+			velX += -10;
 		} else {
-			x = 0;
+			posX = 0;
 		}
 	}
 	if(event.keyCode == 38){
 		console.log("Up");
-		if(y = canvas.height - height){
-			y += -200;
-		} else {
-			y = 0;
+		if(posY == canvas.height - height){
+			jump();
 		}
 	}
-	draw();
 }
 
-const gravity = () =>{
-	const gravityEngine = setInterval(function(){
-		console.log("Gravity 1");
-		if(y < canvas.height - height){
-			y += 5;
-			console.log("Gravity 2");
-		} else if(y >= canvas.height){
-			y = canvas.height;
-			console.log("Gravity 3");
-		}
-		draw();
-	}, 10);
-}
-
-draw();
-gravity();
+renderPage();
 document.onkeydown = move;
